cfcc7c08a4c98c759f0813665f6aee866a315e28
zoarc <157649642+pa-ticket-driver@users.noreply.github.com>
2024-02-07 08:59:40 -0800

    reworked

--- js/script.js
+++ js/script.js
@@ -928,15 +928,15 @@ function scrollWin(x, y) {
 
 document.addEventListener('input', function(event) {
     if (event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') {
-        var inputText = ' ' + event.target.value + ' ';
+        var inputText = event.target.value;
         var words = inputText.split(' ');
         for (var i = 0; i < words.length; i++) {
             if (words[i] === 'pa' || words[i] === 'Pa' || words[i] === 'pA') {
                 words[i] = 'PA';
-            } else if (words[i] === 'pennie') {
+            } else if ( words[i] === 'pennie') {
                 words[i] = 'Pennie';
-            }
+            }     
         }
-        event.target.value = words.join(' ').trim();
+        event.target.value = words.join(' ');
     }
 });
